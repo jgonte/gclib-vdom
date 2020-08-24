293f779dd73ce0c39ddbf196d61b98d67f348ff9
jgonte <jgonte@gmail.com>
2020-08-24 11:30:57 -0400

    Added bundling

---
+++ src/gclib-vdom.ts
@@ -0,0 +1,11 @@
+import VirtualNode from './VirtualNode';
+import createElement from './createElement';
+import ElementPatches from './ElementPatches';
+import diff from './diff';
+
+export {
+    VirtualNode,
+    createElement,
+    ElementPatches,
+    diff
+}
